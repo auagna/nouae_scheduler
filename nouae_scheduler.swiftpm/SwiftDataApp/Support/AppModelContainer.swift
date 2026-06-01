51b20879c6c31336afe419180265771e7bb99030
auagna <yujin54374@gmail.com>
2026-06-02 00:25:13 +0900

    Add SwiftData ModelContainer factory

---
+++ nouae_scheduler.swiftpm/SwiftDataApp/Support/AppModelContainer.swift
@@ -0,0 +1,22 @@
+import SwiftData
+
+enum AppModelContainer {
+    static func make(isStoredInMemoryOnly: Bool = false) throws -> ModelContainer {
+        let schema = Schema([
+            Project.self,
+            RawTask.self,
+            WorkBlock.self,
+            ProjectLog.self,
+            ProjectMemoSection.self,
+            NextAdjustment.self
+        ])
+        let configuration = ModelConfiguration(
+            schema: schema,
+            isStoredInMemoryOnly: isStoredInMemoryOnly
+        )
+        return try ModelContainer(
+            for: schema,
+            configurations: [configuration]
+        )
+    }
+}
