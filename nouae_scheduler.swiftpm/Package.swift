a7cb3a12e924b4ffa3afae026f35a1f1ca9a2127
auagna <yujin54374@gmail.com>
2026-06-02 21:46:10 +0900

    Bump package build for Phase 7 8 9

--- nouae_scheduler.swiftpm/Package.swift
+++ nouae_scheduler.swiftpm/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             bundleIdentifier: "com.auagna.nouae-scheduler.phase123",
             teamIdentifier: "",
             displayVersion: "1.0",
-            bundleVersion: "6",
+            bundleVersion: "9",
             appIcon: .placeholder(icon: .calendar),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [.pad, .phone],
