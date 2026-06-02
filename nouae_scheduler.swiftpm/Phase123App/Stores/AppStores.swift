8da02fc9c79a66570f22ab04985e5ec83110cf10
auagna <yujin54374@gmail.com>
2026-06-02 21:02:15 +0900

    Phase 123 compose stores

---
+++ nouae_scheduler.swiftpm/Phase123App/Stores/AppStores.swift
@@ -0,0 +1,18 @@
+import SwiftData
+
+@MainActor
+final class AppStores {
+    let projectStore: ProjectStore
+    let rawTaskStore: RawTaskStore
+    let workBlockStore: WorkBlockStore
+    let logStore: LogStore
+    let adjustmentStore: NextAdjustmentStore
+
+    init(context: ModelContext) {
+        projectStore = ProjectStore(context: context)
+        rawTaskStore = RawTaskStore(context: context)
+        workBlockStore = WorkBlockStore(context: context)
+        logStore = LogStore(context: context)
+        adjustmentStore = NextAdjustmentStore(context: context)
+    }
+}
