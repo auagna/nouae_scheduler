1913bcddfb315827b7f95648471f2a98649b9b23
auagna <yujin54374@gmail.com>
2026-06-02 21:00:39 +0900

    Phase 123 add Project model

---
+++ nouae_scheduler.swiftpm/Phase123App/Models/Project.swift
@@ -0,0 +1,39 @@
+import Foundation
+import SwiftData
+
+@Model
+final class Project {
+    @Attribute(.unique) var id: UUID
+    var title: String
+    var typeRawValue: String
+    var statusRawValue: String
+    var goal: String
+    var calendarIdentifier: String?
+    var calendarTitle: String?
+    var calendarColorHex: String?
+    var syncStateRawValue: String
+    var createdAt: Date
+    var updatedAt: Date
+    var archivedAt: Date?
+
+    init(id: UUID = UUID(), title: String, type: ProjectType = .personal, status: ProjectStatus = .planning, goal: String = "", calendarIdentifier: String? = nil, calendarTitle: String? = nil, calendarColorHex: String? = nil, syncState: SyncState = .local, createdAt: Date = Date(), updatedAt: Date = Date(), archivedAt: Date? = nil) {
+        self.id = id
+        self.title = title
+        typeRawValue = type.rawValue
+        statusRawValue = status.rawValue
+        self.goal = goal
+        self.calendarIdentifier = calendarIdentifier
+        self.calendarTitle = calendarTitle
+        self.calendarColorHex = calendarColorHex
+        syncStateRawValue = syncState.rawValue
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.archivedAt = archivedAt
+    }
+}
+
+extension Project {
+    var type: ProjectType { get { ProjectType(rawValue: typeRawValue) ?? .personal } set { typeRawValue = newValue.rawValue } }
+    var status: ProjectStatus { get { ProjectStatus(rawValue: statusRawValue) ?? .planning } set { statusRawValue = newValue.rawValue } }
+    var syncState: SyncState { get { SyncState(rawValue: syncStateRawValue) ?? .local } set { syncStateRawValue = newValue.rawValue } }
+}
