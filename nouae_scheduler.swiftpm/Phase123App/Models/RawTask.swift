a776026892ff758435832893ecfc975979ebabfc
auagna <yujin54374@gmail.com>
2026-06-02 21:00:44 +0900

    Phase 123 add RawTask model

---
+++ nouae_scheduler.swiftpm/Phase123App/Models/RawTask.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftData
+
+@Model
+final class RawTask {
+    @Attribute(.unique) var id: UUID
+    var title: String
+    var projectId: UUID?
+    var reminderIdentifier: String?
+    var createdAt: Date
+    var scheduledAt: Date?
+    var isConvertedToBlock: Bool
+    var syncStateRawValue: String
+
+    init(id: UUID = UUID(), title: String, projectId: UUID? = nil, reminderIdentifier: String? = nil, createdAt: Date = Date(), scheduledAt: Date? = nil, isConvertedToBlock: Bool = false, syncState: SyncState = .local) {
+        self.id = id
+        self.title = title
+        self.projectId = projectId
+        self.reminderIdentifier = reminderIdentifier
+        self.createdAt = createdAt
+        self.scheduledAt = scheduledAt
+        self.isConvertedToBlock = isConvertedToBlock
+        syncStateRawValue = syncState.rawValue
+    }
+}
+
+extension RawTask {
+    var syncState: SyncState { get { SyncState(rawValue: syncStateRawValue) ?? .local } set { syncStateRawValue = newValue.rawValue } }
+}
