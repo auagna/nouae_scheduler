7da63c663a0f7ea8b1870cf4dd3b15fffbdb0e89
auagna <yujin54374@gmail.com>
2026-06-04 20:57:55 +0900

    feat: introduce ProjectArea data foundation

--- nouae_scheduler.swiftpm/Package.swift
+++ nouae_scheduler.swiftpm/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             bundleIdentifier: "com.auagna.nouae-scheduler.phase123",
             teamIdentifier: "",
             displayVersion: "1.0",
-            bundleVersion: "15",
+            bundleVersion: "16",
             appIcon: .placeholder(icon: .calendar),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [.pad, .phone],
