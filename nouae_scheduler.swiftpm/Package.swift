1474748ac0a02e9dae230f45fe508ebfa1f9ba02
auagna <yujin54374@gmail.com>
2026-06-04 21:24:16 +0900

    Wire BLOCK Calendar and Reminder routing

--- nouae_scheduler.swiftpm/Package.swift
+++ nouae_scheduler.swiftpm/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             bundleIdentifier: "com.auagna.nouae-scheduler.phase123",
             teamIdentifier: "",
             displayVersion: "1.0",
-            bundleVersion: "17",
+            bundleVersion: "18",
             appIcon: .placeholder(icon: .calendar),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [.pad, .phone],
