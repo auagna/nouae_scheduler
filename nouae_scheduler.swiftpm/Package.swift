17966c0648f72d73e9d6bab2702a507572415757
auagna <yujin54374@gmail.com>
2026-06-11 21:03:08 +0900

    Fix onboarding crash and complete calendar Step B

--- nouae_scheduler.swiftpm/Package.swift
+++ nouae_scheduler.swiftpm/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             bundleIdentifier: "com.auagna.nouae-scheduler.phase123",
             teamIdentifier: "",
             displayVersion: "1.0",
-            bundleVersion: "36",
+            bundleVersion: "37",
             appIcon: .placeholder(icon: .calendar),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [.pad, .phone],
