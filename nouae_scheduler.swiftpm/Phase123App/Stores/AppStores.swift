081ca0ba6946f8df1e6c88613f7dfd09ee140e17
auagna <yujin54374@gmail.com>
2026-06-13 00:25:18 +0900

    Integrate L7 audit fixes and onboarding log flow

--- nouae_scheduler.swiftpm/Phase123App/Stores/AppStores.swift
+++ nouae_scheduler.swiftpm/Phase123App/Stores/AppStores.swift
@@ -9,6 +9,7 @@ final class AppStores: ObservableObject {
     let workBlockStore: WorkBlockStore
     let logStore: LogStore
     let projectNoteStore: ProjectNoteStore
+    let projectBoardStore: ProjectBoardStore
     let adjustmentStore: NextAdjustmentStore
     let dashboardStore: DashboardStore
 
@@ -19,6 +20,7 @@ final class AppStores: ObservableObject {
         workBlockStore = WorkBlockStore(context: context)
         logStore = LogStore(context: context)
         projectNoteStore = ProjectNoteStore(context: context)
+        projectBoardStore = ProjectBoardStore(context: context)
         adjustmentStore = NextAdjustmentStore(context: context)
         dashboardStore = DashboardStore(context: context)
     }
