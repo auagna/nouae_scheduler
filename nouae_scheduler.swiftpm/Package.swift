8e8c95dcecc64b8607ae9f8376511e8628dd8492
auagna <yujin54374@gmail.com>
2026-06-24 22:28:44 +0900

    Add Routine occurrence workflow for Plan

--- nouae_scheduler.swiftpm/Package.swift
+++ nouae_scheduler.swiftpm/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             bundleIdentifier: "com.auagna.nouae-scheduler.phase123",
             teamIdentifier: "",
             displayVersion: "1.0",
-            bundleVersion: "40",
+            bundleVersion: "41",
             appIcon: .placeholder(icon: .calendar),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [.pad, .phone],
