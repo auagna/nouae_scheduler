a8923e057f976313016ad96795e4a1c5f263daed
auagna <yujin54374@gmail.com>
2026-06-26 01:56:41 +0900

    chore: bump package for MVP lock build

--- nouae_scheduler.swiftpm/Package.swift
+++ nouae_scheduler.swiftpm/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             bundleIdentifier: "com.auagna.nouae-scheduler.phase123",
             teamIdentifier: "",
             displayVersion: "1.0",
-            bundleVersion: "52",
+            bundleVersion: "53",
             appIcon: .placeholder(icon: .calendar),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [.pad, .phone],
