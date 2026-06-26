2696f81c46e01eb84cb2acecb31bb0251b6d4539
auagna <yujin54374@gmail.com>
2026-06-27 01:30:41 +0900

    chore: bump package for direct startup fix

--- nouae_scheduler.swiftpm/Package.swift
+++ nouae_scheduler.swiftpm/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             bundleIdentifier: "com.auagna.nouae-scheduler.phase123",
             teamIdentifier: "",
             displayVersion: "1.0",
-            bundleVersion: "54",
+            bundleVersion: "55",
             appIcon: .placeholder(icon: .calendar),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [.pad, .phone],
